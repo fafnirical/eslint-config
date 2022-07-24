83ee1840b6ee65b7ddecac7516a6dc9cc5da884a
Wade Burelbach <fafnirical@users.noreply.github.com>
2022-07-23 17:45:22 -0700

    ci: lint js/ts files with typescript and typescript-eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,22 @@
-/** @type {import('eslint').Linter.Config<import('eslint/rules').ESLintRules>} */
+/** @type {import('eslint').Linter.Config<import('eslint/rules').ESLintRules> & import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
+  ],
   env: {
     node: true,
     es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json'],
   },
   reportUnusedDisableDirectives: true,
   overrides: [
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,11 @@
 const prettier = 'prettier --ignore-unknown --write';
 
 export default {
-  '*, !package.json, !*.{js,mjs,cjs}': prettier,
-  '*.{js,mjs,cjs}': ['eslint --fix', prettier],
+  '*, !package.json, !*.{js,mjs,cjs,ts,mts,cts}': prettier,
+  '*.{js,mjs,cjs,ts,mts,cts}': [
+    () => 'tsc --project tsconfig.eslint.json',
+    'eslint --fix',
+    prettier,
+  ],
   'package.json': ['sort-package-json', prettier],
 };
