d9a754d3573333b13a6f051e5694c1ca79e5da0c
Wade Burelbach <fafnirical@users.noreply.github.com>
2022-09-02 10:56:20 -0700

    ci: use built eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,16 @@
 /** @type {import('eslint').Linter.Config<import('eslint/rules').ESLintRules> & import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   root: true,
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/strict',
-    'prettier',
-  ],
+  extends: ['./dist/base.cjs', './dist/typescript.cjs'],
   env: {
     node: true,
     es2022: true,
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.eslint.json'],
+    project: ['./tsconfig.eslint.json', './tsconfig.build.json'],
   },
   reportUnusedDisableDirectives: true,
-  overrides: [
-    {
-      // This override acts only as a list of file extensions to lint.
-      files: ['*.js', '*.mjs', '*.cjs', '*.ts', '*.mts', '*.cts'],
-    },
-  ],
 };
 
 module.exports = config;
