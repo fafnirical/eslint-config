6974a68d954ebad33455146179777b3723c39b79
Wade Burelbach <3250671+fafnirical@users.noreply.github.com>
2024-05-22 23:50:57 -0700

    feat: Enable eslint-config-prettier default rules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import javascript from './javascript.js';
+import prettier from './prettier.js';
 import type { Config } from './types.js';
 import typescript from './typescript.js';
 
-const config: Config[] = [...javascript, ...typescript];
+const config: Config[] = [...javascript, ...typescript, ...prettier];
 
 export default config;
---
+++ src/prettier.ts
@@ -0,0 +1,6 @@
+import prettier from 'eslint-config-prettier';
+import type { Config } from './types.js';
+
+const config: Config[] = [prettier];
+
+export default config;
