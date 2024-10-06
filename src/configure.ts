3304e78f9e30063a90ec7229a91de59eee739f02
Wade Burelbach <3250671+fafnirical@users.noreply.github.com>
2024-10-06 11:58:14 -0700

    feat: Add configuration options for enabling/disabling certain configs (#58)

--- src/configure.ts
+++ src/configure.ts
@@ -2,33 +2,52 @@ import type { Config } from './types/index.js';
 
 import { getDependencies, hasDependency } from './utils/dependencies.js';
 
-export default async function configure(): Promise<Config[]> {
+interface Options {
+  gitignore?: boolean;
+  prettier?: boolean;
+  typescript?: boolean;
+}
+
+export default async function configure(
+  options: Options = {},
+): Promise<Config[]> {
   const dependencies = getDependencies();
 
-  return [
-    (await import('eslint-config-flat-gitignore')).default(),
+  const {
+    gitignore: enableGitignore = true,
+    prettier: enablePrettier = hasDependency(dependencies, 'prettier'),
+    typescript: enableTypescript = hasDependency(dependencies, 'typescript'),
+  } = options;
+
+  const configs: Config[] = [];
+
+  if (enableGitignore) {
+    configs.push(...(await import('./configs/gitignore.js')).default);
+  }
+
+  configs.push(
     {
       linterOptions: {
         reportUnusedDisableDirectives: true,
       },
     },
-    ...(await import('./configs/comments.js')).default,
-
     ...(await import('./configs/javascript.js')).default,
-
-    ...(hasDependency(dependencies, 'typescript') ?
-      (await import('./configs/typescript.js')).default
-    : []),
-
+    ...(await import('./configs/comments.js')).default,
     ...(await import('./configs/imports.js')).default,
-    ...(hasDependency(dependencies, 'typescript') ?
-      (await import('./configs/imports.js')).typescriptConfig
-    : []),
 
     ...(await import('./configs/perfectionist.js')).default,
+  );
 
-    ...(hasDependency(dependencies, 'prettier') ?
-      (await import('./configs/prettier.js')).default
-    : []),
-  ];
+  if (enableTypescript) {
+    configs.push(
+      ...(await import('./configs/typescript.js')).default,
+      ...(await import('./configs/imports.js')).typescriptConfig,
+    );
+  }
+
+  if (enablePrettier) {
+    configs.push(...(await import('./configs/prettier.js')).default);
+  }
+
+  return configs;
 }
